93e67cb964dc430f2c22083da2c46e071c4d6037
ian otieno <ianotieno23@gmail.com>
2023-09-27 06:47:53 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SurgicalFormComponent } from './components/surgical-form/surgical-form.component';
+import { FormComponent } from './components/form/form.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SurgicalFormComponent,
+    FormComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/surgical-form/surgical-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-surgical-form',
+  templateUrl: './surgical-form.component.html',
+  styleUrls: ['./surgical-form.component.css']
+})
+export class SurgicalFormComponent {
+
+}
