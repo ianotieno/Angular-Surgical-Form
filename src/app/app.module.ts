53da8904957d6dd38aa5a155f7ca7c6da2488a3f
ian otieno <ianotieno23@gmail.com>
2023-09-27 07:06:13 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,45 @@ import { SurgicalFormComponent } from './components/surgical-form/surgical-form.
 import { FormComponent } from './components/form/form.component';
 import { HeaderComponent } from './components/header/header.component';
 
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    SurgicalFormComponent,
+    HeaderComponent,
     FormComponent,
-    HeaderComponent
+    SurgicalFormComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatToolbarModule,
+    MatIconModule,
     AppRoutingModule,
-    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
